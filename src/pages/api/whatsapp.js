6ba8a5b4b18376277ed60c8458e9c8545340a2cb
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-06-01 19:08:30 -0300

    fix: Path in 'whatsapp.js'

--- src/pages/api/whatsapp.js
+++ src/pages/api/whatsapp.js
@@ -1,5 +1,7 @@
+import path from 'path';
 import dotenv from 'dotenv';
-dotenv.config({ path: '../../../.env.development' });
+
+dotenv.config({ path: path.join(process.cwd(), '.env.development') });
 
 export const prerender = false;
 
